a0fc369f13a9b78863019f10b055f2af71d3f9c7
Avinash Vyas <avinash.vyas@nokia.com>
2018-05-10 13:13:09 -0400

    Forgot the __init() file

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+import requests
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
 from mycroft.util.log import getLogger
@@ -6,10 +7,21 @@ from mycroft.util.log import getLogger
 class PptControllerSkill(MycroftSkill):
     def __init__(self):
 	super(PptControllerSkill, self).__init__(name="PptControllerSkill")
+	self.rest_endpoint = "http://135.104.238.160"
 
-    @intent_handler(IntentBuilder().require('PptController'))
+    @intent_handler(IntentBuilder("PPTIntent").require('PptController'))
     def handle_ppt_controller(self, message):
         self.speak_dialog('ppt.controller')
+
+    @intent_handler(IntentBuilder("OpenPPTIntent").require('OpenPPT'))
+    def handle_ppt_controller(self, message):
+	// Send a rest request
+        self.speak_dialog('ppt.open')
+
+    @intent_handler(IntentBuilder("ClosePPTIntent").require('ClosePPT'))
+    def handle_ppt_controller(self, message):
+	// Send a rest request
+        self.speak_dialog('ppt.close')
 
 
 def create_skill():
