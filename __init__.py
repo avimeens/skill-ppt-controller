00861908259544a4302b99a1199c9a5771b5c6cb
Avinash Vyas <avinash.vyas@nokia.com>
2018-05-10 20:14:29 -0400

    Changed log message

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ class PptControllerSkill(MycroftSkill):
     @intent_handler(IntentBuilder("OpenPPTIntent").require('OpenPPT').require("Filename"))
     def handle_ppt_open(self, message):
 	filename = message.data.get("Filename")
-	self.enclosure.mouth_text("filename " + filename)
+	self.enclosure.mouth_text("Nova opening file " + filename)
 	# Send a rest request
         self.speak_dialog('ppt.open')
 
