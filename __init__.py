7743430c6ebdc713cacfc5e16f6f91564d6fc5e1
Avinash Vyas <avinash.vyas@nokia.com>
2018-05-10 20:05:09 -0400

    Changing functino to emit the filename

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ class PptControllerSkill(MycroftSkill):
     @intent_handler(IntentBuilder("OpenPPTIntent").require('OpenPPT').require("Filename"))
     def handle_ppt_open(self, message):
 	filename = message.data.get("Filename")
-	self.emitter.emit(Message("filename " + filename))
+	self.emitter.mouth_text("filename " + filename)
 	# Send a rest request
         self.speak_dialog('ppt.open')
 
